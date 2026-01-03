a5b75e5da684f7c2d55684ac0908066e6a36699e
G. Amparo <giovannefamparo@gmail.com>
2026-01-03 19:23:31 -0300

    logica de aprovacao/reprovacao com if-else

---
+++ cpp/situacao_aluno.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    float nota1 = 8.5;
+    float nota2 = 4.5;
+    float media = (nota1 + nota2) / 2;
+
+    std::cout << "Media final: " << media << std::endl;
+
+    if (media >= 6.0) {
+        std::cout << "Status: APROVADO na Unisanta! Parabens." << std::endl;
+    } else {
+        std::cout << "Status: REPROVADO. Precisa estudar mais para a DP." << std::endl;
+    }
+
+    return 0;
+}
