f58d3700a7164a62964f5120a1042b376f65d696
G. Amparo <giovannefamparo@gmail.com>
2026-02-06 16:03:12 -0300

    Update student grades and output message

--- cpp/media_notas.cpp
+++ cpp/media_notas.cpp
@@ -2,12 +2,12 @@
 
 int main() {
     
-    float nota1 = 9.0;
-    float nota2 = 7.0;
+    float nota1 = 8.0;
+    float nota2 = 6.0;
     
     float media = (nota1 + nota2) / 2;
     
-    std::cout << "A media final do aluno eh: " << media << std::endl;
+    std::cout << "A media final do aluno é: " << media << std::endl;
     
     return 0;
 }
